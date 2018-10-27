40fd48b30449c32f26e1099852c6c0ee884825d2
hlmya <hlmya92@gmail.com>
2018-10-27 23:04:42 +0200

    serializable

---
+++ eclipse-workspace/serialization/src/pr/BookStore.java
@@ -0,0 +1,65 @@
+package pr;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class BookStore {
+	static Map<String, Book> bookMap = new HashMap<String, Book>();
+	
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		addSomeBooks();
+		saveBooks("book.bin");
+		loadBooks("book.bin");
+	}
+	
+	// Puts some predefined books into the map, to serve as initial data.
+	public static void addSomeBooks() {
+		bookMap.put("Math",new Book("Math", "Anh"));
+		bookMap.put("English", new Book("English", "Hoa"));
+	}
+
+	// Writes the books into the given file using serialization.
+	// Returns whether the save operation was successful.
+	public static void saveBooks(String fileName) throws IOException {
+		try(FileOutputStream fos = new FileOutputStream(fileName)) {
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			
+			oos.writeObject(bookMap);
+			
+			oos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Save not successfully");
+		}
+		System.out.println("Save successfully");
+	}
+
+	// Loads the books from the given file.
+	// Returns whether the load operation was successful.
+	@SuppressWarnings("unchecked")
+	public static void loadBooks(String fileName) throws IOException, ClassNotFoundException{
+		try(FileInputStream fis = new FileInputStream(fileName)) {
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			
+			bookMap = (Map<String, Book>) ois.readObject();
+			
+			ois.close();
+			
+			System.out.println(bookMap);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//?
+	public static Optional<Book> get(String title){
+		return Optional.ofNullable(bookMap.get(title));
+	}
+}
---
+++ eclipse-workspace/serialization/src/pr/BookStoreTest.java
@@ -0,0 +1,33 @@
+package pr;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BookStoreTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		// delete save.data
+	}
+	
+	@Test
+	void test() throws IOException, ClassNotFoundException {
+		BookStore store1 = new BookStore();
+		store1.addSomeBooks();
+		store1.saveBooks("helloBooks.bin");
+		
+		BookStore store2 = new BookStore();
+		store2.loadBooks("helloBooks.bin");
+		//
+		Optional<Book> b = store2.get("Math");
+		assertTrue(b.isPresent());
+		assertEquals("Anh", b.get().getAuthor());
+	}
+
+}
+
